294b7be394aefb8fcbb33a07327bcefd6f44779c
Jimmy Oliva <jimmy.oliva@cit.hn>
2024-04-01 16:05:33 -0600

    feat (server): methods get, post, put and delete

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -1,14 +1,85 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" });
+  //   res.json({ message: "success!" });
+  const notes = await prisma.note.findMany();
+
+  res.json(notes);
 });
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("Title and content fields are required");
+    //validation the user is sending the complete info
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops something went wrong");
+    //in case there is any DB error this messagge will be send
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("Id must be a valid number");
+  }
+
+   if (!title || !content) {
+     return res.status(400).send("Title and content fields are required");
+     //validation the user is sending the complete info
+   }
+  try {
+    const updatedNote = await prisma.note.update({
+        where: {id},
+        data: {title, content},
+    });
+
+    res.json(updatedNote);
+
+  } catch (error) {
+    res
+    .status(500)
+    .send("Oops, something went wrong")
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res)=>{
+    const id = parseInt(req.params.id);
+
+    if(!id || isNaN(id)){
+        return res.status(400).send("Id value must be a number")
+    }
+
+    try {
+        await prisma.note.delete({
+            where: {id}
+        })
+        res.status(204).send();
+        
+    } catch (error) {
+        res.status(500).send("Oops, something went wrong");
+        
+    }
+})
 
 app.listen(5000, () => {
   console.log("server running on localhost:5000");
